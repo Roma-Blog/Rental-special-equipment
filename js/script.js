4b9d38d048ca99305db4867be5ef077263dae8ee
Roman <ryzikov14@gmail.com>
2023-10-23 12:22:06 +0700

    js faq

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-$('.faq__question-btn').on('click', function(){
-    let heightEl = $('.faq__question-btn + .faq__answer-box + .faq__answer').css('height')
-    console.log(heightEl)
-    $('.faq__question-btn + .faq__answer-box').css( 'height', heightEl)
-});
+$('.faq__item').each(function(){
+    let heightEl = $(this).find('.faq__question-btn + .faq__answer-box .faq__answer').css('height')
+    const answerBox = $(this).find('.faq__question-btn + .faq__answer-box')
+    const iconArrow = $(this).find('.faq__question-btn .faq__question-icon')
+    const faqBtn = $(this).find('.faq__question-btn')
+    faqBtn.on('click', function(){
+        if ($(this).hasClass('faq-open')){
+            answerBox.css('height', '0px')
+            iconArrow.css('rotate', '0deg')
+            $(this).removeClass('faq-open')
+        }
+        else{
+            answerBox.css('height', heightEl)
+            iconArrow.css('rotate', '90deg')
+            $(this).addClass('faq-open')
+        }
+    })
+})
