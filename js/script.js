c319d30a3abb670c0cb304ca417148e13bbe806f
roma-developer <ryzikov16@gmail.com>
2023-10-26 07:34:37 +0700

    js mask

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+//mask-phone________________________________
+$('.art-stranger').mask('+7 (999) 999-99-99');
+
+        $.fn.setCursorPosition = function(pos) {
+        if ($(this).get(0).setSelectionRange) {
+            $(this).get(0).setSelectionRange(pos, pos);
+        } else if ($(this).get(0).createTextRange) {
+            var range = $(this).get(0).createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+        };
+
+        $('input[type="tel"]').click(function(){
+            $(this).setCursorPosition(4); 
+        });
+
+
 //FAQ________________________________
 $('.faq__item').each(function(){
     let heightEl = $(this).find('.faq__question-btn + .faq__answer-box .faq__answer').css('height')
